a801db04bfaddd8eef87e1ceb47b3d602b28f56a
Manas1820 <manasgupta1820@gmail.com>
2021-02-28 11:07:00 +0530

    OnBording/Rules Linked

--- lib/Screen/home.dart
+++ lib/Screen/home.dart
@@ -1,6 +1,7 @@
 import 'package:MAP/Constants.dart';
 import 'package:MAP/Screen/WaitingRoom.dart';
 import 'package:MAP/Services/NewGameServices.dart';
+import 'package:MAP/onboarding.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +11,8 @@ class CreateRooms extends StatefulWidget {
 }
 
 class _CreateRoomsState extends State<CreateRooms> {
+  TextEditingController mycontroller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +22,30 @@ class _CreateRoomsState extends State<CreateRooms> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: TextField(
+                controller: mycontroller,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blueAccent,
+                    ),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  fillColor: Colors.white,
+                  labelText: 'Enter the Game Code',
+                  hintText: 'Faree',
+                  hintStyle: TextStyle(
+                    color: Colors.purple,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
             Center(
               child: OutlineButton(
                 onPressed: () async {
@@ -27,7 +54,7 @@ class _CreateRoomsState extends State<CreateRooms> {
                   String id = Constants.prefs.getString('userId');
                   String name = Constants.prefs.getString('name');
                   doc.set({
-                    'roomName': 'alibaba',
+                    'roomName': mycontroller.text.toString(),
                     'roomId': doc.id,
                     'leader': Constants.prefs.getString('userId'),
                     'playerId': FieldValue.arrayUnion([id]),
@@ -54,8 +81,19 @@ class _CreateRoomsState extends State<CreateRooms> {
             SizedBox(height: 30.0),
             Center(
               child: OutlineButton(
-                onPressed: () {
-                  print('Received click');
+                onPressed: () async {
+                  if (FirebaseFirestore.instance
+                          .collection('games')
+                          .doc(Text(mycontroller.text).toString()) !=
+                      null)
+                    await NewGameServices()
+                        .joinGame(mycontroller.text.toString());
+
+                  await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => WaitingRoom(
+                              roomId: mycontroller.text.toString())));
                 },
                 child: Text(
                   'Join a Game',
@@ -72,7 +110,10 @@ class _CreateRoomsState extends State<CreateRooms> {
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {},
+        onPressed: () async {
+          await Navigator.push(context,
+              MaterialPageRoute(builder: (context) => OnBoardingPage()));
+        },
         label: Text(
           'Rules',
           style: TextStyle(
--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -1,5 +1,7 @@
 // import 'package:Runbhumi/utils/Constants.dart';
 // import 'package:Runbhumi/view/views.dart';
+import 'package:MAP/Constants.dart';
+import 'package:MAP/Screen/oAuth.dart';
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 
@@ -12,10 +14,10 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
   final introKey = GlobalKey<IntroductionScreenState>();
 
   void _onIntroEnd(context) {
-    // Constants.prefs.setString("firsttime", "done");
-    // Navigator.of(context).pushReplacement(
-    //   MaterialPageRoute(builder: (_) => GauthPage()),
-    // );
+    Constants.prefs.setString("firsttime", "done");
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (_) => GauthPage()),
+    );
   }
 
   Widget _buildImage(String assetName) {
@@ -29,7 +31,11 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
   Widget build(BuildContext context) {
     const bodyStyle = TextStyle(fontSize: 19.0, color: Colors.white);
     const pageDecoration = const PageDecoration(
-      titleTextStyle: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w700, color: Colors.white, fontFamily: "Nightmare2"),
+      titleTextStyle: TextStyle(
+          fontSize: 30.0,
+          fontWeight: FontWeight.w700,
+          color: Colors.white,
+          fontFamily: "Nightmare2"),
       bodyTextStyle: bodyStyle,
       descriptionPadding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
       pageColor: Colors.black,
@@ -42,21 +48,21 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
         PageViewModel(
           title: "Create or join a game",
           body:
-          "Atmost 20 people can play in a game of 4 imposters and atmost 10 people in a game of 2.\n\n And then the game is on!",
+              "Atmost 20 people can play in a game of 4 imposters and atmost 10 people in a game of 2.\n\n And then the game is on!",
           image: _buildImage("board1"),
           decoration: pageDecoration,
         ),
         PageViewModel(
           title: "If you are the IMPOSTER",
           body:
-          "1. Fake your tasks. \n2. When you are in the vicinity of another player you will get a notif if you want to kill him/her. The accept option will send a notif on the players’ phone and he will be directed outside of the game circle.",
+              "1. Fake your tasks. \n2. When you are in the vicinity of another player you will get a notif if you want to kill him/her. The accept option will send a notif on the players’ phone and he will be directed outside of the game circle.",
           image: _buildImage('board2'),
           decoration: pageDecoration,
         ),
         PageViewModel(
           title: "If you are a PLAYER",
           body:
-          "1. Complete your tasks. \n2.If you get kicked off the game, you again need to rush out of the game circle on the map. Once you are out, the others will get a notif that you weren’t the imposter.\n3. If you die by the imposter, you will get a notif that you are dead and you need to rush out of the playing circle on the map. You need to follow a dedicated path, that will appear on the screen and not talk to anyone on your way to tell them about the imposter or you will remove the fun of the game!",
+              "1. Complete your tasks. \n2.If you get kicked off the game, you again need to rush out of the game circle on the map. Once you are out, the others will get a notif that you weren’t the imposter.\n3. If you die by the imposter, you will get a notif that you are dead and you need to rush out of the playing circle on the map. You need to follow a dedicated path, that will appear on the screen and not talk to anyone on your way to tell them about the imposter or you will remove the fun of the game!",
           image: _buildImage('board3'),
           // footer: Button(
           //   myColor: Theme.of(context).primaryColor,
@@ -69,7 +75,8 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
         ),
         PageViewModel(
           title: "Tasks",
-          body: "Number of Tasks \n=\nNumber of people playing\n\nThe tasks currently are standing in that place for a certain amount of time, but in the future we plan on giving you a small game to play during that time.",
+          body:
+              "Number of Tasks \n=\nNumber of people playing\n\nThe tasks currently are standing in that place for a certain amount of time, but in the future we plan on giving you a small game to play during that time.",
           image: _buildImage('board4'),
           decoration: pageDecoration,
         ),
@@ -82,20 +89,21 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
         PageViewModel(
           title: "Who wins?",
           body:
-          "IMPOSTERS win if even one of them survives till the end (the number of PLAYERS are less than or equal to the number of imposters).\n\nPLAYERS win if, They complete all of their tasks or they kick out all the imposters. ",
+              "IMPOSTERS win if even one of them survives till the end (the number of PLAYERS are less than or equal to the number of imposters).\n\nPLAYERS win if, They complete all of their tasks or they kick out all the imposters. ",
           image: _buildImage('board6'),
           decoration: pageDecoration,
         ),
-
       ],
       onDone: () => _onIntroEnd(context),
       //onSkip: () => _onIntroEnd(context), // You can override onSkip callback
       showSkipButton: true,
       skipFlex: 0,
       nextFlex: 0,
-      skip: const Text('Skip', style: TextStyle(fontWeight: FontWeight.w600, color: Colors.white)),
+      skip: const Text('Skip',
+          style: TextStyle(fontWeight: FontWeight.w600, color: Colors.white)),
       next: const Icon(Icons.arrow_forward),
-      done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600, color: Colors.white)),
+      done: const Text('Done',
+          style: TextStyle(fontWeight: FontWeight.w600, color: Colors.white)),
       dotsDecorator: const DotsDecorator(
         size: Size(10.0, 10.0),
         activeColor: Color(0xff2DADC2),
