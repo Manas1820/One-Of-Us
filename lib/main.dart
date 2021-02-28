6dafb0c6f496052b0996ab1b7662e68646044475
Manas1820 <manasgupta1820@gmail.com>
2021-02-28 10:53:57 +0530

    ✨ Changes

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
       home: GauthPage(),
     );
   }
